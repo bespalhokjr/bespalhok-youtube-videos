92c499464d20e6020d05a143e605944a86451638
Paulo <paulojuniore@gmail.com>
2022-05-15 12:20:07 -0300

    removido msg do log clickou do script

--- bespalhok-youtube-videos/js/bespalhok_youtubevideos_script.js
+++ bespalhok-youtube-videos/js/bespalhok_youtubevideos_script.js
@@ -6,8 +6,6 @@ function videosYoutube(){
 		jQuery(this).html(`
 			<iframe class="fitvidsignore" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" width="640" height="360" src="https://www.youtube.com/embed/${id}?controls=1&rel=0&playsinline=0&modestbranding=0&autoplay=1&enablejsapi=1&mute=0"></iframe>
 		`);
-
-		console.log('clickou');
 	});
 }
 
